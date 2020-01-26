a2b838930eee16cfa52bcd95056797f32aa51889
Annie Huang <annie.hn@gmail.com>
2020-01-26 17:22:30 +1100

    Create setup and draw function. You can see draw is constantly called.

---
+++ script.js
@@ -0,0 +1,13 @@
+function setup() {
+    console.log('setup');
+    createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+    // You can see draw is constantly called. This makes it very easy to do animation.
+    console.log('draw');
+
+}
+
+
+
