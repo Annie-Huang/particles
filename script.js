a45975b6ccfb2567b5552923fc3d8aa220905d93
Annie Huang <annie.hn@gmail.com>
2020-01-26 18:08:57 +1100

    Create a Particle class so we can draw a white dot on a ramdon position on the page.

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
+let p;
+
 function setup() {
-    console.log('setup');
+    // console.log('setup');
     createCanvas(window.innerWidth, window.innerHeight);
+    // console.log(random(100));
+
+    p = new Particle();
 }
+
 
 function draw() {
     // // You can see draw is constantly called. This makes it very easy to do animation.
     // console.log('draw');
 
-    // You can uncomment this. Very cool effect!
-    if (mouseIsPressed) {
-        console.log(mouseX, mouseY);
-        fill(0);
-    } else {
-        fill(100);
+    // // You can uncomment this. Very cool effect!
+    // if (mouseIsPressed) {
+    //     console.log(mouseX, mouseY);
+    //     fill(0);
+    // } else {
+    //     fill(100);
+    // }
+    //
+    // // fill(0);
+    // // circle(150, 150, 80);
+    // circle(mouseX, mouseY, 80);
+
+    p.draw();
+}
+
+class Particle {
+    constructor() {
+        // Position
+        this.pos = createVector(random(width), random(height));
+        // Size
+        this.size = 10;
     }
 
-    // fill(0);
-    // circle(150, 150, 80);
-    circle(mouseX, mouseY, 80);
+    draw() {
+        noStroke();
+        fill('rgba(255,255,255,0.5');
+        circle(this.pos.x, this.pos.y, this.size);
+    }
 }
 
 
