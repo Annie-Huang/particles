32eeaad9d726c54255467031972734f5daa14da0
Annie Huang <annie.hn@gmail.com>
2020-01-26 17:25:42 +1100

    Very cool effect. mouse move or click+mousemove create two different circles colors and continue to draw according to your mouse movement

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function setup() {
 }
 
 function draw() {
-    // You can see draw is constantly called. This makes it very easy to do animation.
-    console.log('draw');
+    // // You can see draw is constantly called. This makes it very easy to do animation.
+    // console.log('draw');
 
-    fill(0);
+    // You can uncomment this. Very cool effect!
+    if (mouseIsPressed) {
+        console.log(mouseX, mouseY);
+        fill(0);
+    } else {
+        fill(100);
+    }
+
+    // fill(0);
     // circle(150, 150, 80);
     circle(mouseX, mouseY, 80);
 }
